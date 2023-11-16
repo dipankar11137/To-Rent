49708565eecd203ed3161b6cd1d95496b723a758
dipankar-halder <dipankar15-11137@diu.edu.bd>
2023-11-16 12:03:07 +0600

    add home

--- src/Components/Pages/Home/HomePage.js
+++ src/Components/Pages/Home/HomePage.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const HomePage = () => {
+  const [category, setCategory] = useState('');
   return (
-    <div className='mx-20 grid grid-cols-12'>
-      <div className='col-span-2 border-r-2 border-black'>
-        <button>Family</button>
-        <button>Bachelor</button>
+    <div className="mx-20 shadow-red-500 shadow-r-lg">
+      <div className="grid grid-cols-12">
+        <div className="col-span-2  mt-10 pr-4 border-r-2 ">
+          <h1
+            onClick={() => setCategory('Family')}
+            className={
+              category === 'Family'
+                ? ' text-primary  font-semibold text-2xl border-r-4 border-spacing-6 border-primary cursor-pointer'
+                : ' font-semibold text-2xl cursor-pointer'
+            }
+          >
+            Family
+          </h1>
+          <br />
+          <button>Bachelor</button>
+          <button>category</button>
+        </div>
+        <div className="col-span-10  w-full pl-10">page </div>
       </div>
-      <div className='col-span-10  w-full pl-10'>page </div>
     </div>
   );
 };
