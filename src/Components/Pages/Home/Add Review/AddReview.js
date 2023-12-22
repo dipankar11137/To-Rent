21c3e6dd0f2597df69aa29bc021812398dbad4f0
dipankar-halder <dipankar15-11137@diu.edu.bd>
2023-12-22 19:16:00 +0600

    add review

--- src/Components/Pages/Home/Add Review/AddReview.js
+++ src/Components/Pages/Home/Add Review/AddReview.js
@@ -7,6 +7,7 @@ const AddReview = ({ id }) => {
   const [user] = useAuthState(auth);
   const email = user?.email;
   const [review, setReview] = useState('');
+  const [rating, setRating] = useState('');
   //   const name = user?.displayName;
   //   const img = user?.photoURL;
 
@@ -25,8 +26,8 @@ const AddReview = ({ id }) => {
   }
 
   const onSubmit = () => {
-    const updateReview = { review, id };
-
+    const updateReview = { review, rating, id };
+    console.log(updateReview);
     toast.success('Successfully Add Review');
     const url = `http://localhost:5000/addReview`;
     fetch(url, {
@@ -42,36 +43,63 @@ const AddReview = ({ id }) => {
   return (
     <div className=" modal" role="dialog">
       <div className="modal-box  w-96">
-        <div className=" fixed modal-action -top-9 z-20 -right-0">
+        <div className=" fixed modal-action -top-7 z-20 -right-0">
           <label
             htmlFor="my_modal_6"
             className=" text-red-600  text-3xl font-extrabold px-3 py-1 rounded-full shadow-2xl cursor-pointer"
           >
-            x
+            X
           </label>
         </div>
         <div className="flex justify-center ">
           <div className=" w-96  rounded-lg   ">
-            <label className="label">
+            <label className="label ">
               <span className="label-text font-bold text-lime-700 text-2xl mb-2">
                 Add a Review
               </span>
             </label>
+            <select
+              onChange={e => setRating(e.target.value)}
+              className="select select-bordered w-full max-w-xs bg-white mb-3"
+            >
+              <option disabled selected>
+                Select Rating
+              </option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
             <textarea
+              onChange={e => setReview(e.target.value)}
               type="text"
               placeholder="Your Review"
               className="input input-bordered bg-white w-full max-w-xs h-32"
             />
 
-            <div className="modal-action">
-              <label
-                onClick={onSubmit}
-                htmlFor="my_modal_6"
-                className="w-full btn"
-              >
-                Review
-              </label>
-            </div>
+            {review && rating ? (
+              <div className="modal-action">
+                <label
+                  onClick={onSubmit}
+                  htmlFor="my_modal_6"
+                  className="w-full btn"
+                >
+                  Review
+                </label>
+              </div>
+            ) : (
+              <div className="modal-action">
+                <label
+                  disabled
+                  onClick={onSubmit}
+                  htmlFor="my_modal_6"
+                  className="w-full btn"
+                >
+                  Review
+                </label>
+              </div>
+            )}
           </div>
         </div>
       </div>
